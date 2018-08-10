e1f317122bfaff2a5898165a84351395f2bece03
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2018-08-10 21:09:20 +0200

    asssociate home view model with home page

--- Xchange/ViewModels/HomeViewModel.cs
+++ Xchange/ViewModels/HomeViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 namespace Xchange.ViewModels
 {
-    public class HomeViewModel
+    public class HomeViewModel : BaseViewModel
     {
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { SetValue(ref email, value); }
+        }
+
         public HomeViewModel()
         {
+            
         }
     }
 }
